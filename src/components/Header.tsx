1f4977272a8846ad7cd0217e80abb14ae5d0975f
reversonthaiam <reversonthayan@gmail.com>
2021-12-13 14:07:48 -0300

    Header pronto

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import { Flex, Text, Input, Icon } from '@chakra-ui/react'
+import { RiSearchLine } from 'react-icons/ri'
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1488}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        DashGo
+        <Text as="span" ml="1" color="pink.500">
+          .
+        </Text>
+      </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="grau.50"
+          variant="unstyled"
+          placeholder="Buscar na plataforma"
+          _placeholder={{ color: 'gray.400' }}
+        ></Input>
+        <Icon as={RiSearchLine} fontSize="20"></Icon>
+      </Flex>
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-import Head from 'next/head'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-   <h1>OLá</h1>
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input type="email" name="email" label="E-mail"></Input>
+          <Input type="password" name="password" label="Password"></Input>
+        </Stack>
+        <Button type="submit" mt="6" size="lg" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
