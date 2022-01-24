7c4f4d1bdc419cbb047d8cfe3802fb31551ea7e5
reverson.santos <reverson.santos@frotabank.com.br>
2022-01-24 17:53:27 -0300

    fim dia

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,26 @@
 import { Box, Button, Stack } from '@chakra-ui/react'
 import { PaginationItem } from './PaginationItem'
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number
+  registersPerPage?: number
+  currentPage?: number
+  onPageChange?: (page: number) => void
+}
+
+const siblingsCount = 1;
+
+export function Pagination({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage)
+
+  const previousPages = currentPage > 1
+  ? [...new Array(siblingsCount)].
+
   return (
     <Stack
       direction="row"
@@ -14,10 +33,10 @@ export function Pagination() {
         <strong>0</strong> - <strong> 10 </strong> de <strong> 100 </strong>
       </Box>
       <Stack direction="row" spacing="2">
-          <PaginationItem number={1} isCurrent />
-          <PaginationItem number={2} />
-          <PaginationItem number={3} />
-          <PaginationItem number={4} />
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
       </Stack>
     </Stack>
   )
